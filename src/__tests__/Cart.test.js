578ac134762545ebe8ef6dd79f9d640845719537
Shin Sergey <shin.sergey.t@gmail.com>
2022-12-17 02:42:02 +0600

    17.12

--- src/__tests__/Cart.test.js
+++ src/__tests__/Cart.test.js
@@ -1,6 +1,7 @@
 import Cart from '../service/Cart';
 
 test('new card should be empty', () => {
-    const cart = newCart();
-    expect(cart.items.length).toBe(0);
+  const cart = new Cart();
+
+  expect(cart.items.length).toBe(0);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
+
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
